1adfb6e110c4a495c43f04d812a6ab23788709db
csergey8 <c.sergey@protonmail.com>
2017-09-04 13:47:58 +0300

    add exit command

--- script.js
+++ script.js
@@ -1,51 +1,60 @@
 //using of IFFY for private
 
-(function(){
+(function () {
 
-//Build function contructor
+  //Build function contructor
 
-function Question(question, answers, correct) {
-  this.question = question;
-  this.answers = answers;
-  this.correct = correct;
-}
-
-//Add prototype method
-
-Question.prototype.displayQuestion = function () {
-  console.log(this.question);
-
-  for (var i = 0; i < this.answers.length; i++) {
-    console.log(i + ') ' + this.answers[i]);
+  function Question(question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
   }
-};
 
-//Add another prorotype method
+  //Add prototype method
 
-Question.prototype.checkAnswer = function(ans) {
-  if ( ans === this.correct){
-    console.log('Answer correct');
-  } else {
-    console.log('Wrong answer');
+  Question.prototype.displayQuestion = function () {
+    console.log(this.question);
+
+    for (var i = 0; i < this.answers.length; i++) {
+      console.log(i + ') ' + this.answers[i]);
+    }
+  };
+
+  //Add another prorotype method
+
+  Question.prototype.checkAnswer = function (ans) {
+    if (ans === this.correct) {
+      console.log('Answer correct');
+    } else {
+      console.log('Wrong answer');
+    }
   }
-}
 
-var q1 = new Question('Is JavaScript the coolest programming language in the world?', ['Yes', 'No'], 0);
+  var q1 = new Question('Is JavaScript the coolest programming language in the world?', ['Yes', 'No'], 0);
 
-var q2 = new Question('What\'s the name of this courese\'s teacher?', ['John', 'Michael', 'Jonas'], 2);
+  var q2 = new Question('What\'s the name of this courese\'s teacher?', ['John', 'Michael', 'Jonas'], 2);
 
-var q3 = new Question('What does best decribe coding?', ['Boring', 'Hard', 'Fun', 'Tediuos'], 2);
+  var q3 = new Question('What does best decribe coding?', ['Boring', 'Hard', 'Fun', 'Tediuos'], 2);
 
-var questions = [q1, q2, q3];
+  function nextQuestion() {
+    var questions = [q1, q2, q3];
 
-var n = Math.floor((Math.random() * questions.length));
+    var n = Math.floor((Math.random() * questions.length));
 
-questions[n].displayQuestion();
+    questions[n].displayQuestion();
 
-// Add promt
-var answer = parseInt(prompt('Please select the correct answer.'));
+    // Add promt
+    var answer = prompt('Please select the correct answer.');
 
-questions[n].checkAnswer(answer);
+    questions[n].checkAnswer(answer);
+
+    if (answer !== 'exit') {
+      questions[n].checkAnswer(parseInt(answer));
+      nextQuestion();
+    }
+  }
+
+  nextQuestion();
+
 
 })();
-
