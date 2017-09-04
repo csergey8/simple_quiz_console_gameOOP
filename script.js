e34210433770d47973b8f57013fb719f9aa096e7
csergey8 <c.sergey@protonmail.com>
2017-09-04 14:21:39 +0300

    end code

--- script.js
+++ script.js
@@ -22,12 +22,21 @@
 
   //Add another prorotype method
 
-  Question.prototype.checkAnswer = function (ans) {
+  Question.prototype.checkAnswer = function (ans, callback) {
+    var sc;
     if (ans === this.correct) {
       console.log('Answer correct');
+      sc = callback(true);
     } else {
       console.log('Wrong answer');
+      sc = callback(false);
     }
+    this.displayScore(sc);
+  }
+
+  Question.prototype.displayScore = function (score) {
+    var br = '<br>';
+    console.log('Your current score: '+ score);
   }
 
   var q1 = new Question('Is JavaScript the coolest programming language in the world?', ['Yes', 'No'], 0);
@@ -36,8 +45,24 @@
 
   var q3 = new Question('What does best decribe coding?', ['Boring', 'Hard', 'Fun', 'Tediuos'], 2);
 
+  var questions = [q1, q2, q3];
+
+  //Add closure
+  function score() {
+    var sc = 0;
+    return function (correct) {
+      if (correct) {
+        sc++;
+      }
+      return sc;
+    }
+  }
+
+  var keepScore = score();
+
+
+
   function nextQuestion() {
-    var questions = [q1, q2, q3];
 
     var n = Math.floor((Math.random() * questions.length));
 
@@ -46,10 +71,9 @@
     // Add promt
     var answer = prompt('Please select the correct answer.');
 
-    questions[n].checkAnswer(answer);
 
     if (answer !== 'exit') {
-      questions[n].checkAnswer(parseInt(answer));
+      questions[n].checkAnswer(parseInt(answer), keepScore);
       nextQuestion();
     }
   }
