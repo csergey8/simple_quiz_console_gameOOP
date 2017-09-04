dac7a4c0afb3f6ddcf687baf536971fc8c30019f
csergey8 <c.sergey@protonmail.com>
2017-09-04 11:51:04 +0300

    add prototype method

--- script.js
+++ script.js
@@ -6,9 +6,22 @@ function Question(question, answers, correct) {
   this.correct = correct;
 }
 
+//Add prototype method
+
+Question.prototype.displayQuestion = function () {
+  console.log(this.question);
+
+  for (var i = 0; i < this.answers.length; i++) {
+    console.log(i + ') ' + this.answers[i]);
+  }
+};
+
 var q1 = new Question('Is JavaScript the coolest programming language in the world?', ['Yes', 'No'], 0);
 
 var q2 = new Question('What\'s the name of this courese\'s teacher?', ['John', 'Michael', 'Jonas'], 2);
 
 var q3 = new Question('What does best decribe coding?', ['Boring', 'Hard', 'Fun', 'Tediuos'], 2);
 
+var questions = [q1, q2, q3];
+
+var n = Math.floor((Math.random() * questions.length));
